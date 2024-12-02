a9c83a39180dff44518518d01e2bfe015c8ddea2
KeremCesur <keremcesur52@gmail.com>
2024-12-02 11:23:51 +0300

    Stock Takip Uygulama
    devamı

---
+++ AMP12_1633_StockTakipProgram/AMP12_1633_StockTakipProgram/Model/Yetki.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMP12_1633_StockTakipProgram.Model
+{
+    public class Yetki
+    {
+    }
+}
